7a012828170b5d60f9f81a55a200aed1d4526f36
Shuang Wu <seanwu1105@gmail.com>
2022-09-06 14:17:17 -0400

    feat(programming-1): calculate word frequency

--- programming/assignment1/interactive_naive_bayes/naive_bayes/preprocessing.py
+++ programming/assignment1/interactive_naive_bayes/naive_bayes/preprocessing.py
@@ -3,6 +3,78 @@
 # 3. Make sure there is no missing data or empty strings (or nan)
 # 4. Get all unique words
 # 5. Transform content into the frequency of each word
+# TODO: Remove stopwords
 # 6. Data would be Dict[int, Dict[str, int]]
 # 7. Transform data to be np.array[int] (len=categories) and np.array[int, int]
 #    (len=[categories, words])
+import dataclasses
+import itertools
+import os
+import string
+from collections import Counter
+
+import numpy as np
+import numpy.typing as npt
+import pandas as pd
+from interactive_naive_bayes.naive_bayes.classifier import Category, WordCount
+
+
+@dataclasses.dataclass
+class ProcessedData:
+    targets: npt.NDArray[Category]
+    target_labels: tuple[str, ...]
+    features: npt.NDArray[WordCount]
+    feature_labels: tuple[str, ...]
+
+
+def get_default_data_path():
+    return os.path.join(os.path.dirname(os.path.realpath(__file__)), "dbpedia_8K.csv")
+
+
+def preprocess(filename=get_default_data_path()) -> ProcessedData:
+    df = pd.read_csv(filename)
+
+    assert df["label"].unique().size == len(TARGET_LABELS)
+
+    df["content"] = (
+        df["content"].map(remove_punctuation).map(lambda s: s.lower().split())
+    )
+
+    feature_labels: tuple[str, ...] = tuple(
+        set(itertools.chain.from_iterable(df["content"].values))
+    )
+
+    feature_indices = {label: i for i, label in enumerate(feature_labels)}
+
+    counters: list[dict[str, int]] = df["content"].map(Counter).to_list()
+
+    features = np.zeros((len(df), len(feature_labels)), dtype=WordCount)
+
+    for i, counter in enumerate(counters):
+        for word, count in counter.items():
+            features[i, feature_indices[word]] = count
+
+    return ProcessedData(
+        targets=df["label"].to_numpy(dtype=np.uint),
+        target_labels=TARGET_LABELS,
+        features=features,
+        feature_labels=feature_labels,
+    )
+
+
+def remove_punctuation(text: str) -> str:
+    return "".join(
+        map(lambda c: c if c in string.ascii_letters + string.digits else " ", text)
+    )
+
+
+TARGET_LABELS: tuple[str, ...] = (
+    "Company",
+    "Education Institution",
+    "Artist",
+    "Athlete",
+    "Office Holder",
+    "Mean Of Transportation",
+    "Building",
+    "Natural Place",
+)
---
+++ programming/assignment1/main.py
@@ -0,0 +1,9 @@
+from interactive_naive_bayes.naive_bayes.preprocessing import preprocess
+
+
+def main():
+    preprocess()
+
+
+if __name__ == "__main__":
+    main()
