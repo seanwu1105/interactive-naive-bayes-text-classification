93051c1b8e02c40c813c6d518596572188aa849c
Shuang Wu <seanwu1105@gmail.com>
2022-09-07 00:32:05 -0400

    feat(programming-1): add empty ui window

---
+++ programming/assignment1/interactive_naive_bayes/ui/main.qml
@@ -0,0 +1,8 @@
+import QtQuick.Controls
+
+ApplicationWindow {
+    visible: true
+    width: 640
+    height: 480
+    title: "Hello World"
+}
--- programming/assignment1/main.py
+++ programming/assignment1/main.py
@@ -1,6 +1,13 @@
-def main():
-    pass
+import sys
 
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtQml import QQmlApplicationEngine
 
 if __name__ == "__main__":
-    main()
+    app = QGuiApplication(sys.argv)
+    engine = QQmlApplicationEngine()
+    engine.load("interactive_naive_bayes/ui/main.qml")
+    rootObjects = engine.rootObjects()
+    if not rootObjects:
+        sys.exit("Engine loading failed")
+    sys.exit(app.exec())
