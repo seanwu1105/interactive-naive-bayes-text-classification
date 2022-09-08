73b139ebefd38249e531e5615015b34c6dde3cbe
Shuang Wu <seanwu1105@gmail.com>
2022-09-08 17:41:57 -0400

    feat(programming-1): add validation

--- programming/assignment1/tests/naive_bayes/test_naive_bayes.py
+++ programming/assignment1/tests/naive_bayes/test_naive_bayes.py
@@ -1,5 +1,6 @@
 from interactive_naive_bayes.naive_bayes.classifier import predict, train
 from interactive_naive_bayes.naive_bayes.preprocessing import preprocess, to_sample
+from interactive_naive_bayes.naive_bayes.validation import validate
 
 
 def test_naive_bayes():
@@ -14,3 +15,13 @@ def test_naive_bayes():
     )
 
     assert processed.target_labels[result] == "Building"
+
+
+# TODO: Finish the test
+def test_validation():
+    processed = preprocess()
+    validate(
+        folds=10,
+        targets=processed.targets,
+        samples=processed.samples,
+    )
