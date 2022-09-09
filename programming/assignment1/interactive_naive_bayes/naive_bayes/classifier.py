1d22b01b78fea2fd6b426bd5e1ac0f4bbe0f462b
Shuang Wu <seanwu1105@gmail.com>
2022-09-09 16:37:37 -0400

    feat(programming-1): use word frequency to train model

--- programming/assignment1/interactive_naive_bayes/naive_bayes/classifier.py
+++ programming/assignment1/interactive_naive_bayes/naive_bayes/classifier.py
@@ -17,16 +17,32 @@ class Model:
 
 def train(categories: npt.NDArray[Category], documents: npt.NDArray[Count]) -> Model:
     assert len(categories) == len(documents)
+    return Model(get_prior(categories), get_likelihood(categories, documents))
 
 
-def get_prior(categories: npt.NDArray[Category]) -> dict[Category, float]:
-    pass
+def get_prior(categories: npt.NDArray[Category]) -> npt.NDArray[np.floating]:
+    unique, counts = np.unique(categories, return_counts=True)
+    prior = np.zeros(len(unique))
+    prior[unique] = counts / len(categories)
+    return prior
 
 
 def get_likelihood(
     categories: npt.NDArray[Category], documents: npt.NDArray[Count]
 ) -> npt.NDArray[np.floating]:
     assert len(categories) == len(documents)
+
+    unique = np.unique(categories)
+
+    category_likelihoods = []
+    for category in unique:
+        indices = np.nonzero(categories == category)
+        word_counts = documents[indices].sum(axis=0)
+        all_counts = word_counts.sum()
+        words_likelihoods = (word_counts + 1) / (all_counts + len(word_counts))
+        category_likelihoods.append(words_likelihoods)
+
+    return np.vstack(category_likelihoods)
 
 
 def predict(document: npt.NDArray[Count], model: Model) -> Category:
--- programming/assignment1/tests/naive_bayes/test_classifier.py
+++ programming/assignment1/tests/naive_bayes/test_classifier.py
@@ -1,1 +1,33 @@
-# TODO: add tests
+import numpy as np
+
+from interactive_naive_bayes.naive_bayes.classifier import train
+
+
+def test_train():
+    categories = np.array([1, 1, 0, 0, 0, 2, 2, 2, 2, 2])
+    documents = np.array(
+        [
+            [1, 0, 0],
+            [0, 1, 0],
+            [0, 0, 1],
+            [1, 1, 0],
+            [1, 0, 1],
+            [2, 1, 1],
+            [1, 2, 1],
+            [1, 1, 2],
+            [2, 1, 2],
+            [1, 2, 2],
+        ]
+    )
+
+    model = train(categories, documents)
+
+    np.testing.assert_allclose(
+        model.prior,
+        np.array([0.3, 0.2, 0.5]),
+    )
+
+    np.testing.assert_allclose(
+        model.likelihood,
+        np.array([[0.375, 0.25, 0.375], [0.4, 0.4, 0.2], [0.32, 0.32, 0.36]]),
+    )
