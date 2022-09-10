fix(programming-1): use log-posterior to avoid underflow

programming/assignment1/interactive_naive_bayes/naive_bayes/classifier.py
@@ -45,17 +45,16 @@ def _get_likelihood(
     return np.vstack(category_likelihoods)
 
 
-def predict(document: npt.NDArray[Count], model: Model) -> tuple[Category, np.floating]:
+def predict(document: npt.NDArray[Count], model: Model) -> tuple[Category, float]:
     posteriors = np.fromiter(
         (
-            prior * np.prod(model.likelihood[category] ** document)
+            np.log(prior) + np.sum(np.log(model.likelihood[category]) * document)
             for category, prior in enumerate(model.prior)
         ),
         dtype=np.float64,
     )
 
-    confidence = np.max(posteriors) / np.sum(
-        posteriors
-    )  # TODO: Use logsumexp to avoid underflow
+    # TODO: Wrong confidence
+    confidence = np.max(posteriors) / np.sum(posteriors)
 
-    return np.argmax(posteriors), confidence
+    return np.argmax(posteriors), float(confidence)

programming/assignment1/tests/naive_bayes/test_classifier.py
@@ -40,4 +40,5 @@ def test_predict():
         likelihood=np.array([[0.5, 0.25, 0.25], [0.25, 0.5, 0.25]]),
     )
 
-    assert predict(document, model) == 1
+    (category, _) = predict(document, model)
+    assert category == 1